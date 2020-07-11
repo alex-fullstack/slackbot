9327fb7b159d9926e755eb3cb23c9295f58d04c0
Aleksandr Bebyakov <alex0181@yandex.ru>
2020-07-11 21:11:00 +0400

    add message send

--- events/views.py
+++ events/views.py
@@ -2,6 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.conf import settings
+from slack import WebClient
+from slack.errors import SlackApiError
+
+SLACK_BOT_USER_TOKEN = getattr(settings, 'SLACK_BOT_USER_TOKEN', None)
+client = WebClient(token=SLACK_BOT_USER_TOKEN)
 
 SLACK_VERIFICATION_TOKEN = getattr(settings, 'SLACK_VERIFICATION_TOKEN', None)
 
@@ -13,4 +18,14 @@ class Events(APIView):
             return Response(status=status.HTTP_403_FORBIDDEN)
         if slack_message.get('type') == 'url_verification':
             return Response(data=slack_message, status=status.HTTP_200_OK)
+        try:
+            response = client.chat_postMessage(
+                channel='#team',
+                text="Hello world!")
+            assert response["message"]["text"] == "Hello world!"
+        except SlackApiError as e:
+            # You will get a SlackApiError if "ok" is False
+            assert e.response["ok"] is False
+            assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
+            print(f"Got an error: {e.response['error']}")
         return Response(status=status.HTTP_200_OK)
